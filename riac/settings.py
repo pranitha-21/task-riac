04ac2230bed08b8fe69bed10a31e5bacfee236e0
Pranitha <pranithapranitha388@gmail.com>
2025-09-03 03:15:04 +0530

    back

--- riac/settings.py
+++ riac/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os,logging, sys
+import logging, sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'riac.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [BASE_DIR / 'task/Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
